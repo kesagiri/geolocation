8f4fb41822dacc5a5529e0ef184798b6aa3ddf0f
kesagiri <antony.rus@yandex.ru>
2018-02-12 21:15:28 +0300

    Change AccountMap cmp

--- force-app/main/default/aura/AccountMap/AccountMapController.js
+++ force-app/main/default/aura/AccountMap/AccountMapController.js
@@ -1,7 +1,7 @@
 ({
    jsLoaded: function(component, event, helper) {
 
-      var map = L.map('map', {zoomControl: false}).setView([37.784173, -122.401557], 14);
+      var map = L.map('map', {zoomControl: false}).setView([55.726522, 37.638573], 14);
       L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
         {
               attribution: 'Tiles © Esri'
